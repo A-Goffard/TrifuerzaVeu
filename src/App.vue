6728949d04fdfae26098d1770cf25d515b9106b5
A-Goffard <aintzagoffard@gmail.com>
2024-03-08 13:11:23 +0100

    cambio de carpetas y borrado de .jsons

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <img class="trifuerza_logo" src="./assets/trifuerza.png" alt="">
+    <Equipo msg="Ongi Etorri!"/>
+  </div>
+</template>
+
+<script>
+import Equipo from './components/Equipo.vue'/* esto es para importar el componenete Equipo.vue */
+
+export default {
+  name: 'App',
+  components: {
+    Equipo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #000000;
+  margin-top: 3rem;
+
+}
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-decoration: none;
+  background-color: rgb(240, 255, 255);
+}
+
+</style>
---
+++ src/components/Equipo.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+        <p>
+            Vamos a hablar sobre los primeros conceptos de Vue
+        </p>
+    </div>
+    
+</template>
+  
+  <script>
+  export default {
+    name: 'Equipo',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+
+  </style>
+  
